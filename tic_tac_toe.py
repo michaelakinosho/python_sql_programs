bbde10c578e4e32f5ed6d0603e7cb798f223eff0
Michael Akinosho <makinosh@ama-assn.org>
2019-07-11 12:28:19 -0500

    Tic Tac Toe program that does not implement classes

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -27,7 +27,6 @@
 
 # **Step 1: Write a function that can print out a board. Set up your board as a list, where each index 1-9 corresponds with a number on a number pad, so you get a 3 by 3 board representation.**
 
-# In[ ]:
 
 
 test_board = ['#','1','2','3','4','5','6','7','8','9']
