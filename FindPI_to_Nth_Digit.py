effe40b7a99570cf451b9d89de1f85297adddff1
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2019-07-12 09:54:40 -0500

    Updated error for out of scope issue

--- FindPI_to_Nth_Digit.py
+++ FindPI_to_Nth_Digit.py
@@ -6,11 +6,36 @@
 #see the Python documentation on Decimal fixed point and floating point arithmetic
 #https://docs.python.org/2/library/decimal.html
 
+#limiting the number of decimal places pi can be returned for
+#even if user enters a number greater than 100
+dec_limit = 100
+
 from math import exp, expm1
+from decimal import *
+import os
 
+def pi_nth_digit():
+    os.system('cls')
+    print("PI is known as an irritational number.\nMeaning it can go to infinity.")
+    print("This program will return a value of pi up to the number of digits you specify")
+    #mPI = Decimal(0)
 
-print("PI is known as an irritational number.\nMeaning it can go to infinity.")
-print("This program will return a value of pi up to the number of digits you specify")
+    try:
+        num_digits = int(input("Number of decimal places for PI:"))
 
-num_digits = input("Number of decimal places for PI:")
-print(num_digits)
+    except Exception:
+
+        print('Please enter a number')
+        num_digits = int(input("Number of decimal places for PI:"))
+
+    if num_digits > dec_limit:
+        num_digits = dec_limit
+    elif num_digits < 3:
+        num_digits = 3
+
+    getcontext().prec = num_digits
+    mPI = Decimal(22)/Decimal(7)
+
+    return mPI
+
+print(pi_nth_digit())
