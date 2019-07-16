99d786357b9d909cd5a67856913d4899f1a25b72
Michael Akinosho <makinosh@ama-assn.org>
2019-07-16 16:21:57 -0500

    Finally updated both parts of Fibonacci program

--- Fibonacci_Sequence.py
+++ Fibonacci_Sequence.py
@@ -1,25 +1,37 @@
+#My signature and contact details
+#Written by: Michael Akinosho
+#Email: michaelakinosho@moaadvisory.com
+
 #This program has two parts
 #The program will number prompt for a Number: num
-#In the first part: is num in the Fibonacci sequence of numbers
-#In the second part: what is the Fibonacci number in the num position
+#In the first part: what is the Fibonacci number in the position entered
+#In the second part: is the position entered in the first part in the Fibonacci sequence of numbers
 #of the Fibonacci sequence
 
 def fibo_num_sequ(num=0):
     fibo_num = 0
     first_fn = 0
     second_fn = 1
+    fibo_num_lst = [first_fn,second_fn]
 
     x = 1
-    print("x is: {}, 1st Fibonacci number is: {}, 2nd Fibonacci number is: {}, New Fibonacci number is {}:".format(x,first_fn,second_fn,first_fn))
+    print("Position {} has Fibonacci number of: {} ".format(x,first_fn))
 
-    while x < num:
-        x += 1
-        print("x is: {}, 1st Fibonacci number is: {}, 2nd Fibonacci number is: {}, New Fibonacci number is {}:".format(x,first_fn,second_fn,second_fn))
+    x += 1
+    print("Position {} has Fibonacci number of: {} ".format(x,second_fn))
+
+    x += 1
+    while len(fibo_num_lst) < num:
         fibo_num = first_fn + second_fn
+        fibo_num_lst.append(fibo_num)
+        print("Position {} has Fibonacci number of: {}".format(x,fibo_num))
         first_fn = second_fn
         second_fn = fibo_num
+        x += 1
 
-    print(fibo_num)
+    if num in fibo_num_lst:
+         print("The position entered above of: {} is also a Fibonacci number at position index of: {} ".format(num, fibo_num_lst.index(num)+1))
+    print(fibo_num_lst)
 
 def input_num():
     num_digits = 0
