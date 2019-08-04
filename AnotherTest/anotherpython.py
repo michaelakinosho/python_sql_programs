285bf107e236b72956882b32ac760725b025900e
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2019-08-04 15:55:13 -0500

    Update

--- AnotherTest/anotherpython.py
+++ AnotherTest/anotherpython.py
@@ -1,2 +1,13 @@
-print(2+5)
-print('Michael Akinosho')
+from PyQt5.QtWidgets import *
+
+app = QApplication([])
+button = QPushButton("Click")
+
+def on_button_clicked():
+    alert = QMessageBox()
+    alert.setText("You clicked the button!")
+    alert.exec()
+    
+button.clicked.connect(on_button_clicked)
+button.show()
+app.exec()
