9f77cac9162e10b417f14e8f3d19598ca3e80bf3
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2019-08-26 08:02:09 -0500

    Working version for binary to decimal converter

--- binary_decimal_converter.py
+++ binary_decimal_converter.py
@@ -1,5 +1,50 @@
-dec_bin_dict = 
-#def dec_bin_conv():
+dec_bin_dict = {"Type":0,"Number":0.0}
+def dec_bin_conv():
+	dec_bin_type()
+	dec_bin_num()
+	print(dec_bin_dict)
+
+	nType = dec_bin_dict["Type"]
+	nNumber = dec_bin_dict["Number"]
+	nResult = "0"
+	nFinResult = ""
+	nPow = 0
+	i = 0
+	iCount = 0
+
+	#nNumber = int(str(nNumber).split(".")[0])
+	nNum_List = str(nNumber).split(".")
+	for y in nNum_List:
+		iCount += 1
+		yNum = int(y)
+		#Converting from decimal to binary
+		if nType == 1:
+			nResult = ""
+			while yNum != 0:
+				nResult = str(int(yNum % 2)) + nResult
+				yNum = (yNum-(yNum % 2))/2
+			print(nResult)
+
+		#Converting from binary to decimal
+		elif nType == 2:
+			
+			nPow = len(str(yNum)) -1
+			i = 0
+			while nPow > -1 :
+				print(yNum)
+				if str(yNum)[i] == "1":
+					#nTemp = (2 ** nPow) + nTemp
+					nResult = (2 ** nPow) + int(nResult)
+				nPow -= 1
+				i += 1
+			print(nResult)
+
+		if iCount == 1:
+			nFinResult = str(nResult)
+		else:
+			nFinResult = str(nFinResult) + str(".") + str(nResult)
+		print(nFinResult)
+		nResult = "0"
 
 
 
@@ -12,6 +57,7 @@ def dec_bin_type():
 		while num_type != 1 and num_type != 2:
 			print("Number entered is {} ".format(num_type))
 			num_type = int(input("Please enter 1 for decimal or enter 2 for binary: "))
+		dec_bin_dict["Type"] = num_type
 
 	except ValueError:
 		print("Please try again, something went wrong")
@@ -23,7 +69,26 @@ def dec_bin_type():
 		print("Thank you!!")
 
 def dec_bin_num():
+	global dec_bin_dict
 	num_val = 0.0
 
 	try:
 		num_val = float(input("Please enter the number to convert: "))
+		if dec_bin_dict["Type"] == 2:
+			for n in str(num_val):
+				if n != "." and n != "0" and n != "1":
+					print("Number {} is not binary".format(num_val))
+					num_val = float(input("Please enter the number to convert: "))
+					break
+		dec_bin_dict["Number"] = num_val
+	
+	except ValueError:
+		print("Please try again, something went wrong")
+		dec_bin_num()
+	except:
+		print("Oops something went wrong")
+
+	finally:
+		print("Thank you!!")
+
+dec_bin_conv()
