fb4d1784177bc9452053e1301385b6b634c6d46f
Michael Akinosho <makinosh@ama-assn.org>
2019-08-26 17:10:42 -0500

    Adding to calculator program

--- moa_calculator.py
+++ moa_calculator.py
@@ -11,7 +11,7 @@ class MainWindow(QMainWindow):
 
 		#Menu
 		self.menu = self.menuBar()
-		self.file_menu = self.menu.addMenu("File")
+		self.home_menu = self.menu.addMenu("Home")
 
 		std_calc_action = QAction("Standard", self)
 		std_calc_action.triggered.connect(self.std_calc)
@@ -19,8 +19,58 @@ class MainWindow(QMainWindow):
 		sci_calc_action = QAction("Scientific", self)
 		sci_calc_action.triggered.connect(self.sci_calc)
 
-		self
+		exit_action = QAction("Exit", self)
+		exit_action.setShortcut(QKeySequence.Quit)
+		exit_action.triggered.connect(self.close)
+
+		self.home_menu.addAction(std_calc_action)
+		self.home_menu.addAction(sci_calc_action)
+		self.home_menu.addAction(exit_action)
+
+		#Status bar
+		self.status = self.statusBar()
+		self.status.showMessage("Calculator opened")
 
 	@Slot()
 	def exit_app(self, checked):
 		QApplication.quit()
+
+class Widget(QWidget):
+	def __init__(self):
+		QWidget.__init__(self)
+
+		self.layout = QHBoxLayout()
+
+		self.TenKeyPad = QGridLayout()
+		self.SciKeyPad = QHBoxLayout()
+
+		self.layout.addWidget(self.TenKeyPad)
+		self.layout.addWidget(self.SciKeyPad)
+
+		#Number 10 Keypad
+		for i in 10:
+			
+		self.bZero = QPushButton("0")
+		self.bOne = QPushButton("1")
+		self.bTwo = QPushButton("2")
+		self.bThree = QPushButton("3")
+		self.bFour = QPushButton("4")
+		self.bFive = QPushButton("5")
+		self.bSix = QPushButton("6")
+		self.bSeven = QPushButton("7")
+		self.bEight = QPushButton("8")
+		self.bNine = QPushButton("9")
+
+if __name__!="__main__":
+	#Qt Application
+	app = QApplication(sys.argv)
+	#QWidget
+	widget = Widget()
+	#QMainWindow using QWidget as central widget
+	window = MainWindow(widget)
+
+	window.resize(800,600)
+	window.show()
+
+	#Execute application
+	sys.exit(app.exec_())
