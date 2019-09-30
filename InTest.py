cee94c8ff47cc9773ee646434c432d8860cd4ecc
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2019-09-30 10:32:24 -0500

    Working on matrix rotation algorithm.

--- InTest.py
+++ InTest.py
@@ -1,7 +1,81 @@
-tStr = 101101
+#!/bin/python3
 
-for n in str(tStr):
-	print(n)
-	if n != "." and n != "0" and n != "1":
-		print("This value {} is greater than 1\n".format(n))
-		break
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the matrixRotation function below.
+def matrixRotation(matrix, m, n, r):
+    num_of_rings = min(m,n)/2
+    matrix_string = ""
+    new_m = 0
+    new_n = 0
+    new_m2 = 0
+    #num_of_poses_ring = (max(m,n)*2) + ((min(m,n)-2)*2)
+
+    i,j = 0,0
+    x = 0
+    while x < num_of_rings:
+
+        z = 0
+        while z < r:
+            q = 0
+            i, j = x,x
+            num_of_poses_ring = (max(m,n)*2) + ((min(m,n)-2)*2)
+            cur_value = matrix[i][j]
+            while q < num_of_poses_ring:
+                new_m = m - 1
+                new_n = new_m + n - 1
+                new_m2 = new_m*2 + new_n
+
+                if (q) < (new_m):
+                    i += 1
+                elif (q) < (new_n):
+                    j += 1
+                elif (q) < (new_m2):
+                    i -= 1
+                else:
+                    j -= 1
+
+                print("In ring: {}, rotation: {}, future position is {},{} of value {}, position counter: {}".format(x,z,i,j,cur_value,q+1))
+                next_value = matrix[i][j]
+                matrix[i][j] = cur_value
+                cur_value = next_value
+
+                q += 1
+
+            z += 1
+        m -= 2
+        n -= 2
+        x += 1
+
+    #print(matrix)
+    #print(i, j)
+
+    i = 0
+    while i < len(matrix):
+        j = 0
+        matrix_string = ""
+        while j < len(matrix[i]):
+            matrix_string += (str(matrix[i][j]) + " ")
+            j += 1
+        print(matrix_string)
+        i += 1
+
+if __name__ == '__main__':
+    mnr = input().rstrip().split()
+
+    m = int(mnr[0])
+
+    n = int(mnr[1])
+
+    r = int(mnr[2])
+
+    matrix = []
+
+    for _ in range(m):
+        matrix.append(list(map(int, input().rstrip().split())))
+
+    matrixRotation(matrix, m, n, r)
