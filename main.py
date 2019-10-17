0a11db9febcdee7c7a6bfdffc4abb6435127fc52
Michael Akinosho <makinosh@ama-assn.org>
2019-10-17 16:32:36 -0500

    Refactored a Python2 program to Python3; range, generators and more list

--- main.py
+++ main.py
@@ -8,26 +8,37 @@ import sys
 
 # Complete the minimumBribes function below.
 def minimumBribes(q,n):
-    cnt,pos,org = list(range(1,n+1)),list(range(1,n+1)),list(range(1,n+1))
-    print(cnt)
-    print(pos)
-    print(org)
+    cnt,pos,org = list([0]*(n+1)),list(range(n+1)),list(range(n+1))
 
     invalid = 0
     ans = 0
-    for i in range(n):
+    for i in range(n-1,-1,-1):
+
+        print("Top of For Loop:")
+        print(cnt)
+        print(pos)
+        print(org)
+        print("Middle of For Loop:")
+
+
         if invalid:
             break
 
+        #Here we pass the queue, starting from the back position n-1
         oldp = pos[q[i]]
         print("oldp: ",oldp, "arr[i]: ", q[i])
 
+
         newp = i + 1
+        print("newp: ",newp)
 
         while oldp != newp:
 
             ans += 1
-
+            print("oldp: ",oldp)
+            print("oldp+1", oldp+1)
+            #print("org[oldp+1]: ",org[oldp+1])
+            #print("cnt[org[oldp+1]]: ",cnt[org[oldp+1]])
             cnt[org[oldp+1]] += 1
 
             if cnt[org[oldp + 1]] > 2:
@@ -39,6 +50,12 @@ def minimumBribes(q,n):
             pos[org[oldp]] = oldp
             pos[org[oldp+1]] = oldp + 1
             print("oldp: ", oldp, "newp: ", newp)
+
+            print(cnt)
+            print(pos)
+            print(org)
+            print("Bottom of For Loop:\n")
+
             oldp += 1
 
     if invalid:
