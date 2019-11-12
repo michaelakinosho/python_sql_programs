a8a478290fd5bde1da16b3ecc0eedf248ed3ef9d
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2019-11-12 11:51:57 -0600

    Working on Magic Square program

--- formingMagicSquare.py
+++ formingMagicSquare.py
@@ -8,25 +8,59 @@ import sys
 
 # Complete the formingMagicSquare function below.
 def formingMagicSquare(s):
+    cost_counter = 0
+    cc_s_sublist_one = 0
+    cc_s_sublist_two = 0
+    even_nums = [2,4,6,8]
+    number_difference = 7
+    even_digit = 0
+    s_sublist = []
 
-    rep_cost = 0
-    evens = [2,4,6,8]
-    odds = [1,3,7,9]
-    print(sum(s[0]))
-    print(s)
+
+    MagicSquares = [
+                    [[8,3,4],[1,5,9],[6,7,2]],
+                    [[8,1,6],[3,5,7],[4,9,2]],
+                    [[6,1,8],[7,5,3],[2,9,4]],
+                    [[6,7,2],[1,5,9],[8,3,4]],
+                    [[4,9,2],[3,5,7],[8,1,6]],
+                    [[4,3,8],[9,5,1],[2,7,6]],
+                    [[2,7,6],[9,5,1],[4,3,8]],
+                    [[2,9,4],[9,5,1],[4,3,8]]]
 
     if s[1][1] != 5:
-        rep_cost = abs(s[1][1] - 5)
+        cost_counter = abs(s[1][1]-5)
+
+    if s[0][0]%2 != 0:
+        for n in even_nums:
+            if abs(s[0][0]-n) == 1:
+                even_digit = n
+                number_difference = 1
+                break
+            elif abs(s[0][0]-n) < even_digit:
+                even_digit = n
+                number_difference = abs(s[0][0]-n)
+        s[0][0] = even_digit
+        #cost_counter += number_difference
+
+    n = 0
+    while n < len(MagicSquares):
+        if MagicSquares[n][0][0] == even_digit:
+            s_sublist.append(MagicSquares[n])
+            s_sublist.append(MagicSquares[n+1])
+        n += 2
 
     i = 0
-    while i < len(s):
-        while j < len(s[i]):
-            if s[i][j] != evens:
+    while i < len(s_sublist[0]):
 
 
-            j += 2
+    print(cost_counter)
 
-        i += 2
+    #print(MagicSquares)
+    #for n in MagicSquares:
+    #    print(n)
+
+    return(0)
+
 
 if __name__ == '__main__':
     #fptr = open(os.environ['OUTPUT_PATH'], 'w')
