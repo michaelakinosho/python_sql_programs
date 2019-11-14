a7c65effbdc1dfaf28bf03300dc57b45e2fa947a
Michael Akinosho <makinosh@ama-assn.org>
2019-11-14 16:54:39 -0600

    Working a challenging Array Manipulation program

--- arrayManipulation_v2.py
+++ arrayManipulation_v2.py
@@ -8,15 +8,28 @@ import sys
 
 # Complete the arrayManipulation function below.
 def arrayManipulation(n, queries):
-    my_lst = [0] * (n+1)
+    counter_cur = 0
+    counter_max = 0
+    queries.sort()
 
-    for x in range(1,n+1):
-        for y in queries:
-            for z in range(y[0],y[1]+1):
-                print(z, y[2])
+    max_num = 0
+    for y in queries:
+        max_num += y[2]
+    print(max_num)
+
+    for n in range(1,n+1):
+        for x in queries:
+            if n >= x[0] and n <= x[1]:
+                counter_cur += x[2]
+        if counter_cur >= max_num:
+            return(counter_cur)
+        if counter_cur > counter_max:
+            counter_max = counter_cur
+        counter_cur = 0
+
             #print(y)
         #print(x)
-
+    return(counter_max)
 
 if __name__ == '__main__':
     #fptr = open(os.environ['OUTPUT_PATH'], 'w')
--- arrayManipulation_v3.py
+++ arrayManipulation_v3.py
@@ -9,19 +9,37 @@ import sys
 # Complete the arrayManipulation function below.
 def arrayManipulation(n, queries):
     max_num = 0
+    counter_cur = 0
+    counter_max = 0
+
+    index_lst = set()
+    for i in queries:
+        index_lst = index_lst.union(range(i[0],i[1]+1))
+    index_lst = list(index_lst)
 
     for y in queries:
         max_num += y[2]
 
-    my_lst = [0] * (n+1)
+    for x in index_lst:
+        #print(x)
+        for y in queries:
+            if x >= y[0] and x <= y[1]:
+                counter_cur += y[2]
+        if counter_cur == max_num:
+            
+            return(counter_cur)
+        elif counter_cur > counter_max:
+            counter_max = counter_cur
+        counter_cur = 0
 
-    for y in queries:
-        for i in range(y[0],y[1]+1):
-            my_lst[i] += y[2]
-            #print(i)
+    return(counter_max)
+
+
+
+
 
     #print(my_lst)
-    return(max(my_lst))
+    #return(max(my_lst))
 
 if __name__ == '__main__':
     #fptr = open(os.environ['OUTPUT_PATH'], 'w')
