ba30494f4ffd1750dc7d243ad0856ccf9c767027
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2019-11-20 15:16:26 -0600

    Working this Hard Anagram solution

--- sherlockAndAnagrams.py
+++ sherlockAndAnagrams.py
@@ -4,17 +4,32 @@ for q_itr in range(q):
 
     s = input()
     my_dict = {}
+    my_list = []
     counter = 0
     s_len = len(s)
     for x in range(s_len):
-        s_count = s.count(s[x])
+        s_char = s[x]
+        s_count = s.count(s_char)
+        print("s count of: ", s_char, " is: ",s_count)
         if s_count > 1:
-            print(s[x])
-            my_dict.update({s[x]:s.index(s[x])})
+            #print(s[x])
+            idx = s.index(s_char)
+            my_list.append(idx)
+            idx += 1
+
+            while len(my_list) < s_count:
+                print(len(my_list))
+                if s[idx] == s_char:
+                    my_list.append(idx)
+
+                print("len of my list: ", len(my_list))
+                my_dict.update({s[x]:my_list})
+                idx += 1
+
 
     #for (x,y) in my_dict.items():
 
 
     #         counter += sum(range(0,y+1))
-    print(counter)
+    #print(counter)
     print(my_dict)
