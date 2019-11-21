3f8a136c733c6ceba049439f42e42ffaf8d2778b
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2019-11-21 15:26:28 -0600

    Working this Hard Anagram solution

--- sherlockAndAnagrams.py
+++ sherlockAndAnagrams.py
@@ -1,6 +1,5 @@
 q = int(input())
 
-
 for q_itr in range(q):
 
     s = input()
@@ -10,35 +9,28 @@ for q_itr in range(q):
 
     s_len = len(s)
     for x in range(s_len):
+        my_list = []
         s_char = s[x]
         s_count = s.count(s_char)
-        print("s count of: ", s_char, " is: ",s_count)
-        if s_count > 1:
+        #print("s count of: ", s_char, " is: ",s_count)
+        if s_count > 1 and my_dict.get(s_char) == None:
             #print(s[x])
             idx = s.index(s_char)
             my_list.append(idx)
             idx += 1
 
             while len(my_list) < s_count:
-                print(len(my_list))
+                #print(len(my_list))
                 if s[idx] == s_char:
                     my_list.append(idx)
 
-                print("len of my list: ", len(my_list))
-                my_dict.update({s[x]:my_list})
+                #print("len of my list: ", len(my_list))
+                #my_dict.update({s[x]:my_list})
                 idx += 1
 
+            my_dict.update({s[x]:my_list})
+    #step 1
+    for n,m in my_dict.items():
+        counter += sum(range(len(m)))
 
-    for x,y in my_dict.items():
-        counter += 0
-        while y < s_len:
-            if s[y-1] == s[y]:
-                counter += 1
-            elif s[y] == x:
-                counter += 1
-
-            y += 1
-
-    #         counter += sum(range(0,y+1))
-    #print(counter)
     print(my_dict)
