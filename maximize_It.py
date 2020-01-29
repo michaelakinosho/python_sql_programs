84fb1b66b99eb9aeef613e950c600ff6a56c157a
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2020-01-29 17:09:37 -0600

    I believe my version works, will try more test cases

--- maximize_It.py
+++ maximize_It.py
@@ -1,7 +1,14 @@
-from itertools import repeat
 
-my_list = [5,7,8,9,10,501]
-fkey = lambda x : x**2%1000
-num_list = list(map(fkey,my_list))
+A,B = map(int,input().split(" "))
 
-print(num_list)
+fkey = lambda x : x**2%B
+max_num = 0
+
+for i in range(A):
+    my_list = list(map(int,input().split(" ")))
+    num_list = list(map(fkey,my_list))
+    print((num_list))
+    print(max(num_list))
+    max_num += max(num_list)
+
+print(max_num)
