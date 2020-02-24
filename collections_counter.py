428040c72f0c200529970c1dd23513f71bc7b231
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2020-02-24 12:57:41 -0600

    Working on collections

--- collections_counter.py
+++ collections_counter.py
@@ -3,7 +3,14 @@ from collections import *
 X = int(input())
 N = Counter(list(map(int,input().split(" "))))
 N = dict(N)
-print(N)
+#print(N)
+earnings = 0
 xi = int(input())
 for _ in range(0,xi):
     s = list(map(int,input().split(" ")))
+    temp = N.get(s[0])
+    if temp != None and temp > 0:
+        earnings += s[1]
+        N.update({s[0]:temp-1})
+        #print(N.get(s[0]))
+print(earnings)
