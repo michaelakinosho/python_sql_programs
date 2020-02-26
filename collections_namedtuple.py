d7f3c3cb40b556500d47d2b619142a6132cf4d36
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2020-02-26 15:54:41 -0600

    Working on collections

--- collections_namedtuple.py
+++ collections_namedtuple.py
@@ -1,4 +1,14 @@
 from collections import namedtuple
 
 N = int(input())
-Student = namedtuple('Student','ID MARKS CLASS NAME')
+Student = namedtuple('Student',input())
+average_mark = 0
+
+for _ in range(0,N):
+    record = input().split(" ")
+    #print(record)
+    stu = Student._make(record)
+    stu = Student(record[0], record[1], record[2], record[3])
+    average_mark += int(stu.MARKS)
+
+print('{:.2f}'.format(average_mark/N))
