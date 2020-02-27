ca09ca8421f2a1f5e07bb7b100b0bfef22809a4d
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2020-02-27 15:10:13 -0600

    Neat collections worked on

--- collections_namedtuple.py
+++ collections_namedtuple.py
@@ -1,15 +1,9 @@
 from collections import namedtuple
 
-N = int(input())
-Student = namedtuple('Student',input())
-average_mark = 0
+N, Student, my_list = int(input()), namedtuple('Student',input()), []
 
 for _ in range(0,N):
-    #record = input().split("\t")
-    #print(record)
-    stu = Student._make(input().split()).MARKS
-    #stu = Student(record[0], record[1], record[2], record[3])
-    #average_mark += int(stu.MARKS)
-    print(stu)
 
-print('{:.2f}'.format(average_mark/N))
+    my_list.append(int(Student._make(input().split()).MARKS))
+
+print('{:.2f}'.format(sum(my_list)/N))
