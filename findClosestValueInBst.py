ce192286b54787f9f79f263f3664076a0f547a47
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2022-02-12 22:33:24 -0600

    Constructing BST
    Implementing BST construction

--- findClosestValueInBst.py
+++ findClosestValueInBst.py
@@ -9,8 +9,23 @@ class BST:
 tree = {"nodes": [{"id": "10", "left": "5", "right": "15", "value": 10},{"id": "15", "left": "13", "right": "22", "value": 15},{"id": "22", "left": None, "right": None, "value": 22},{"id": "13", "left": None, "right": "14", "value": 13},{"id": "14", "left": None, "right": None, "value": 14},{"id": "5", "left": "2", "right": "5-2", "value": 5},{"id": "5-2", "left": None, "right": None, "value": 5},{"id": "2", "left": "1", "right": None, "value": 2},{"id": "1", "left": None, "right": None, "value": 1}],"root": "10"}
 target = 12
 
+# Solution if from AlgoExpert, could not understand the implementation
+# Decided to use solution from AlgoExpert
 def findClosestValueInBst(tree, target):
     # Write your code here.
-    print(tree)
+    return findClosestValueInBstHelper(tree, target, tree.value)
 
-findClosestValueInBst(tree,target)
+def findClosestValueInBstHelper(tree, target, closest):
+    currentNode = tree
+    while currentNode is not None:
+        if abs(target - closest) > abs(target - currentNode.value):
+            closest = currentNode.value
+        if target < currentNode.value:
+            currentNode = currentNode.left
+        elif target > currentNode.value:
+            currentNode = currentNode.right
+        else:
+            break
+    return closest
+
+findClosestValueInBst((tree),target)
