02b8f603c6f77d4021322e98794c6b9ccff487a7
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2022-03-16 14:39:57 -0500

    Completed mergeOverlappingIntervals
    Completed mergeOverlappingIntervals

--- mergeOverlappingIntervals.py
+++ mergeOverlappingIntervals.py
@@ -9,33 +9,34 @@ def mergeOverlappingIntervals(intervals):
         j = i + 1
         while j < arr_len:
             if intervals[i][0] > intervals[j][0]:
-                j += 1
-            else:
-                break
-        intervals.insert(j,intervals[i])
-        intervals.pop(i)
+                print(intervals)
+                tempInterval = intervals[j]
+                intervals[j] = intervals[i]
+                intervals[i] = tempInterval
+                print(intervals)
+            
+            j += 1
+            
+        
+        #intervals.pop(i)
         i += 1
-    
-    #print(intervals)
-    
+        
     i = 0
     while i < arr_len - 1:
         j = i + 1
-        while j < arr_len - 1:
-            if intervals[i][1] > intervals[j][1]:
+        while j < arr_len:
+            if intervals[i][1] >= intervals[j][1]:
                 #print(intervals)
                 intervals.pop(j)
                 arr_len -= 1
                 j -= 1
-            if intervals[i][1] > intervals[j][0]:
+            elif intervals[i][1] >= intervals[j][0]:
                 #print(intervals)
                 intervals[i][1] = intervals[j][1]
                 intervals.pop(j)
                 arr_len -= 1
                 j -= 1
                 
-            
-            
             j += 1
         i += 1
 
@@ -47,5 +48,8 @@ print(mergeOverlappingIntervals(intervals))
 intervals = [[1, 2],[3, 5],[4, 7],[6, 8],[9, 10]]
 print(mergeOverlappingIntervals(intervals))
 
-intervals = [[100,105],[1,104],[2,103]]
+intervals = [[100,105],[1,104]]
+print(mergeOverlappingIntervals(intervals))
+
+intervals = [[2,3],[4,5],[6,7],[8,9],[1,10]]
 print(mergeOverlappingIntervals(intervals))
