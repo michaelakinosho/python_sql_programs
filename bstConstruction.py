acccfe2f5bb30534d883aa29644ada90a2486780
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2022-07-15 00:16:26 -0500

    Working on more system design and algo tutorials

--- bstConstruction.py
+++ bstConstruction.py
@@ -9,29 +9,95 @@ class BST:
         self.right = None
 
     def insert(self, value):
-        
+        currentNode = self
+        while True:
+            if value < currentNode.value: # So it can insert this new node on the left side
+                if currentNode.left is None:
+                    currentNode.left = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.left
+            else: # So it can insert this new node on the right side
+                if currentNode.right is None:
+                    currentNode.right = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.right
+        return self
         # Write your code here.
         # Do not edit the return statement of this method.
         return self
 
-    def insertLeft(self, value):
-        return self
-      
-    def insertRight(self, value):
+    def contains(self, value):
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                currentNode = currentNode.right
+            else:
+                return True
+        return False
+    
+    def remove(self, value, parentNode=None):
+        currentNode = self
+        print(f'Trying to see the node values: {currentNode.value}')
+        while currentNode is not None:
+            if value < currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.right
+            else:
+                if currentNode.left is not None and currentNode.right is not None:
+                    print(f"Current node value is: {currentNode.value}")
+                    currentNode.value = currentNode.right.getMinValue()
+                    print(f"Current node value is: {currentNode.value}")
+                    
+                    currentNode.right.remove(currentNode.value, currentNode)
+                    
+                elif parentNode is None:
+                    if currentNode.left is not None:
+                        currentNode.value = currentNode.left.value
+                        currentNode.right = currentNode.left.right
+                        currentNode.left = currentNode.left.left
+                    elif currentNode.right is not None:
+                        currentNode.value = currentNode.right.value
+                        currentNode.left = currentNode.right.right
+                    else:
+                        pass
+                elif parentNode.left == currentNode:
+                    parentNode.left = currentNode.left if currentNode.left is not None else currentNode.right
+                elif parentNode.right == currentNode:
+                    parentNode.right = currentNode.left if currentNode.left is not None else currentNode.right
+                break
         return self
     
-    def contains(self, value):
-        # Write your code here.
-        pass
+    def getMinValue(self):
+        currentNode = self
+        while currentNode.left is not None:
+            currentNode = currentNode.left
+        return currentNode.value
+        
 
-    def remove(self, value):
-        # Write your code here.
-        # Do not edit the return statement of this method.
-        return self
-
-myBST = BST(10)
-print(myBST.value)
-myBST.insert(23)
+myBST = BST(5)
+myBST.insert(16)
+myBST.insert(9)
+myBST.insert(37)
 myBST.insert(3)
-print(myBST.right)
-print(myBST.left)
+myBST.insert(45)
+myBST.insert(20)
+myBST.insert(21)
+myBST.insert(17)
+print(myBST.value)
+print(myBST.left.value)
+print(myBST.right.value)
+print(myBST.contains(22))
+myBST.remove(5)
+num = myBST.value
+print(num)
+myBST.remove(num)
+num = myBST.value
+print(num)
+print(myBST.right.value)
---
+++ vaidateBst/parsingBst_Json.py
@@ -0,0 +1,25 @@
+x = {
+  "tree": {
+    "nodes": [
+      {"id": "10", "left": "5", "right": "15", "value": 10},
+      {"id": "15", "left": "13", "right": "22", "value": 15},
+      {"id": "22", "left": None, "right": None, "value": 22},
+      {"id": "13", "left": None, "right": "14", "value": 13},
+      {"id": "14", "left": None, "right": None, "value": 14},
+      {"id": "5", "left": "2", "right": "5-2", "value": 5},
+      {"id": "5-2", "left": None, "right": None, "value": 5},
+      {"id": "2", "left": "1", "right": None, "value": 2},
+      {"id": "1", "left": None, "right": None, "value": 1}
+    ],
+    "root": "10"
+  }
+}
+
+y = x["tree"]
+
+print(y)
+
+# print(x["tree"]["nodes"])
+# for k in x["tree"]["nodes"]:
+#     for item in k:
+#         print(item, k[item])
