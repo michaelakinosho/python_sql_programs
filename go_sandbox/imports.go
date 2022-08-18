1bf2013f0f3b5c694611027743b474ec7aac0e53
Michael Akinosho <37967455+michaelakinosho@users.noreply.github.com>
2022-08-18 12:11:46 -0500

    Working on GO

---
+++ go_sandbox/imports.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func testing() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+}
+
+func main() {
+	testing()
+}
---
+++ go_sandbox/type-inference.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := true // change me!
+	w := 314456789
+	fmt.Printf("v is of type %T\nAnd w is of type %T\n", v, w)
+}
